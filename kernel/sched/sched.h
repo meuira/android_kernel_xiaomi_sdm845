3134ae77da18a44d16174c5e387eae7c641cef20
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-06-21 10:04:10 +0100

    sched, drivers: Remove max param from effective_cpu_util()/sched_cpu_util()
    effective_cpu_util() already has a `int cpu' parameter which allows to
    retrieve the CPU capacity scale factor (or maximum CPU capacity) inside
    this function via an arch_scale_cpu_capacity(cpu).
    
    A lot of code calling effective_cpu_util() (or the shim
    sched_cpu_util()) needs the maximum CPU capacity, i.e. it will call
    arch_scale_cpu_capacity() already.
    But not having to pass it into effective_cpu_util() will make the EAS
    wake-up code easier, especially when the maximum CPU capacity reduced
    by the thermal pressure is passed through the EAS wake-up functions.
    
    Due to the asymmetric CPU capacity support of arm/arm64 architectures,
    arch_scale_cpu_capacity(int cpu) is a per-CPU variable read access via
    per_cpu(cpu_scale, cpu) on such a system.
    On all other architectures it is a a compile-time constant
    (SCHED_CAPACITY_SCALE).
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by: Lukasz Luba <lukasz.luba@arm.com>
    Link: https://lkml.kernel.org/r/20220621090414.433602-4-vdonnefort@google.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3401,11 +3401,11 @@ static inline unsigned long cpu_util_freq(int cpu)
 
 #ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
 unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
+				 enum schedutil_type type,
 				 struct task_struct *p);
 #else /* CONFIG_CPU_FREQ_GOV_SCHEDUTIL */
 static inline unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
+				 enum schedutil_type type,
 				 struct task_struct *p)
 {
 	return 0;
