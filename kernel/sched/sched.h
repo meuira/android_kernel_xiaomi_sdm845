3f4b2f2294b11446bb99bab85d1322f83136f882
EmanuelCN <emanuelghub@gmail.com>
2024-05-21 20:28:22 +0300

    sched: Remove sched_avg_update()
    *As per msm-4.19

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2045,8 +2045,6 @@ static inline int hrtick_enabled(struct rq *rq)
 #endif /* CONFIG_SCHED_HRTICK */
 
 #ifdef CONFIG_SMP
-extern void sched_avg_update(struct rq *rq);
-
 #ifndef arch_scale_freq_capacity
 static __always_inline
 unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu)
@@ -2285,7 +2283,6 @@ static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta)
 }
 #else
 static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta) { }
-static inline void sched_avg_update(struct rq *rq) { }
 #endif
 
 extern struct rq *lock_rq_of(struct task_struct *p, struct rq_flags *flags);
