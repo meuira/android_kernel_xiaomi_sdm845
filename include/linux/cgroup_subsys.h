kvsnr113/xiaomi_sm8250_kernel

sched: add dummy schedtune to satisfy userspace compability
without this, some rom throw errors even preventing it from booting normally as per known case with new a16 based roms.
so this should fix the issue without having schedtune because we are using uclamp.

Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

include/linux/cgroup_subsys.h
@@ -21,7 +21,7 @@ SUBSYS(cpu)
 SUBSYS(cpuacct)
 #endif
 
-#if IS_ENABLED(CONFIG_SCHED_TUNE)
+#if IS_ENABLED(CONFIG_SCHED_TUNE) || IS_ENABLED(CONFIG_SCHED_TUNE_DUMMY)
 SUBSYS(schedtune)
 #endif
 

meuira/android_kernel_xiaomi_sdm845

include/linux/cgroup_subsys.h
@@ -20,7 +20,7 @@ SUBSYS(cpu)
 SUBSYS(cpuacct)
 #endif
 
-#if IS_ENABLED(CONFIG_CGROUP_SCHEDTUNE)
+#if IS_ENABLED(CONFIG_CGROUP_SCHEDTUNE) || IS_ENABLED(CONFIG_SCHED_TUNE_DUMMY)
 SUBSYS(schedtune)
 #endif
 